96003f47f0c8c1cea8bda7cf64a89bc778c00341
Nahuel Benitez <nahueljosebenitez7@gmail.com>
2025-01-28 19:17:18 -0300

    configuración inicial terminada

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+
+const PORT = 8080
+
+app.listen(PORT, ()=>{
+    console.log("server corriendo en http://localhost:8080");
+})
+
+app.get("/ping", (req, res )=> {
+    console.log("pong")
+    res.send("pong")
+})
